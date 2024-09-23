959d3bfc999cbe6df55e7d5a43f34a383b7a455a
Sejal Rai <sejalrai23@gmail.com>
2024-09-24 01:32:13 +0530

    Added Logging System

---
+++ Problems/loggingSystem/ErrorLogProcessor.java
@@ -0,0 +1,20 @@
+package Problems.loggingSystem;
+
+public class ErrorLogProcessor extends LogProcessor{
+
+  ErrorLogProcessor(LogProcessor nexLogProcessor){
+      super(nexLogProcessor);
+  }
+
+  public void log(int logLevel,String message){
+
+      if(logLevel == ERROR) {
+          System.out.println("ERROR: " + message);
+      } else{
+
+          super.log(logLevel, message);
+      }
+
+  }
+}
+
