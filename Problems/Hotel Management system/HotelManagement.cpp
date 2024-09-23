c73224b728b5ca180b9f3d029a6bf41dbbe61cf3
Sejal Rai <sejalrai23@gmail.com>
2024-09-23 15:56:49 +0530

    Initial commit

---
+++ Problems/Hotel Management system/HotelManagement.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Hotel {
+  string Name;
+  int id;
+  Location hotelLocation;
+  vector<Room> roomList;
+}
+
+class Location {
+  int pin;
+  string street;
+  string area;
+  string city;
+  string country;
+}
+
+
+class Room {
+  string roomNumber;
+  RoomStyle roomStyle;
+  RoomStatus roomStatus;
+  double bookingPrice;
+  vector<RoomKey> roomKeys;
+  vector<HouseKeepingLog> houseKeepingLogs;
+}
+
+enum RoomStyle {
+    STANDARD,
+    DELUXE,
+    FAMILY_SUITE
+};
+
+
+enum RoomStatus {
+  AVAILABLE , RESERVED , NOT_AVAILABLE , OCCUPIED , SERVICE_IN_PROGRESS
+}
+
+class RoomKey {
+  string keyId;
+  string barCode;
+  bool isActive;
+  bool isMaster
+  // Date issuedAt
+  public:
+    void assignRoom(Room room);
+  
+}
+
+class HouseKeepingLog {
+  string description;
+  // Date startDate;
+  int duration;
+  HouseKeeper houseKeeper;
+  public : 
+    void addRoom(Room room);
+}
+
+
+class Person {
+  public:
+    virtual void fun() = 0;
+  protected:
+    string name;
+    Account accountDetails;
+    string phone;
+}
+
+class Account {
+  string username;
+  string password;
+  AccountStatus accountStatus;
+}
+
+enum AccountStatus {
+  ACTIVE , CLOSED , BLOCKED;
+}
+
+class HouseKeeper : public Person {
+  public:
+    vector<Room> getRoomService(string date);
+}
+
+class Search {
+  public:
+    vector<Room> searchRoom(RoomStyle roomStyle , string startDate , int duration);
+}
+
+class Booking{
+  public:
+    RoomBooking createBooking(Guest guestInfo);
+    RoomBooking cancelBooking (int bookingId);
+}
+
+
+class Guest : public Person {
+  Search searchObj;
+  Booking bookingObj;
+  public:
+    vector<RoomBooking> getAllRoomBookings();
+}
+
+
+class Receptionist : public Person {
+  Search searchObj;
+  Booking bookingObj;
+  public:
+    void checkInGuest(Guest guest , RoomBooking bookingInfo);
+    void checkOutGuest(Guest guest , RoomBooking bookingInfo);
+}
+
+
+class Admin : public Person {
+  public:
+    void addRoom (Room roomDetail);
+    Room deleteRoom(string roomId);
+    void editRoom(Room roomDetail);
+}
+
+
+class RoomBooking {
+  string bookingId;
+  string startDate;
+  int durationInDays;
+  BookingStatus bookingStatus;
+  vector<Guest> guestList;
+  vector<Room> roomInfo;
+  BaseRoomCharge totalRoomCharge;
+}
+
+// Decorator Pattern is used to decorate prices here
+interface BaseRoomCharge {
+  Double getCost();
+}
+
+class RoomCharge implements BaseRoomCharge {
+  double cost;
+  Double getCost(){
+    return cost;
+  }
+  void setCost(double cost){
+    this.cost = cost;
+  }
+}
+
+class RoomServiceCharge implements BaseRoomCharge{
+  double cost;
+  BaseRoomCharge baseRoomCharge;
+  Double getCost(){
+    baseRoomCharge.setCost(baseRoomCharge.getCost() + cost);
+    return baseRoomCharge.getCost();
+  }
+}
---
+++ Problems/Hotel Management system/HotelManagementSystem.java
@@ -0,0 +1,256 @@
+import java.util.Date;
+import java.util.List;
+
+// Enumerations
+enum RoomStyle {
+    STANDARD, DELUXE, FAMILY_SUITE
+}
+
+enum RoomStatus {
+    AVAILABLE, RESERVED, NOT_AVAILABLE, OCCUPIED, SERVICE_IN_PROGRESS
+}
+
+enum AccountStatus {
+    ACTIVE, CLOSED, BLOCKED
+}
+
+// Location class
+class Location {
+    Double longitude;
+    Double latitude;
+    
+    Location(Double longitude, Double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+}
+
+// Account class
+class Account {
+    String username;
+    String password;
+    AccountStatus accountStatus;
+    
+    Account(String username, String password, AccountStatus accountStatus) {
+        this.username = username;
+        this.password = password;
+        this.accountStatus = accountStatus;
+    }
+}
+
+// Room class
+class Room {
+    String roomNumber;
+    RoomStyle roomStyle;
+    RoomStatus roomStatus;
+    Double bookingPrice;
+    List<RoomKey> roomKeys;
+    List<HouseKeepingLog> houseKeepingLogs;
+}
+
+// RoomKey class
+class RoomKey {
+    String keyId;
+    String barCode;
+    Date issuedAt;
+    Boolean isActive;
+    Boolean isMaster;
+    
+    void assignRoom(Room room) {
+        // Implementation
+    }
+}
+
+// HouseKeepingLog class
+class HouseKeepingLog {
+    String description;
+    Date startDate;
+    int duration;
+    HouseKeeper housekeeper;
+    
+    void addRoom(Room room) {
+        // Implementation
+    }
+}
+
+// Person abstract class
+abstract class Person {
+    String name;
+    Account accountDetail;
+    String phone;
+    
+    abstract void fun();
+}
+
+// HouseKeeper class
+class HouseKeeper extends Person {
+    List<Room> getRoomsServiced(Date date) {
+        // Implementation
+        return null;
+    }
+    
+    @Override
+    void fun() {
+        // Implementation
+    }
+}
+
+// Guest class
+class Guest extends Person {
+    Search searchObj;
+    Booking bookingObj;
+    
+    List<RoomBooking> getAllRoomBookings() {
+        // Implementation
+        return null;
+    }
+    
+    RoomBooking createBooking() {
+        // Implementation
+        return null;
+    }
+    
+    RoomBooking cancelBooking(int bookingId) {
+        // Implementation
+        return null;
+    }
+    
+    @Override
+    void fun() {
+        // Implementation
+    }
+}
+
+// Receptionist class
+class Receptionist extends Person {
+    Search searchObj;
+    Booking bookingObj;
+    
+    void checkInGuest(Guest guest, RoomBooking bookingInfo) {
+        // Implementation
+    }
+    
+    void checkOutGuest(Guest guest, RoomBooking bookingInfo) {
+        // Implementation
+    }
+    
+    @Override
+    void fun() {
+        // Implementation
+    }
+}
+
+// Admin class
+class Admin extends Person {
+    void addRoom(Room roomDetail) {
+        // Implementation
+    }
+    
+    Room deleteRoom(String roomId) {
+        // Implementation
+        return null;
+    }
+    
+    void editRoom(Room roomDetail) {
+        // Implementation
+    }
+    
+    @Override
+    void fun() {
+        // Implementation
+    }
+}
+
+// Search class
+class Search {
+    List<Room> searchRoom(RoomStyle roomStyle, Date startDate, int duration) {
+        // Implementation
+        return null;
+    }
+}
+
+// RoomBooking class
+class RoomBooking {
+    String bookingId;
+    Date startDate;
+    int durationInDays;
+    List<Guest> guestList;
+    List<Room> roomInfo;
+    BaseRoomCharge totalRoomCharges;
+}
+
+// BaseRoomCharge interface
+interface BaseRoomCharge {
+    Double getCost();
+
+    void setCost(double d);
+}
+
+// RoomCharge class
+class RoomCharge implements BaseRoomCharge {
+    double cost;
+
+    @Override
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
+
+// RoomServiceCharge class
+class RoomServiceCharge implements BaseRoomCharge {
+    double cost;
+    BaseRoomCharge baseRoomCharge;
+
+    @Override
+    public Double getCost() {
+        baseRoomCharge.setCost(baseRoomCharge.getCost() + cost);
+        return baseRoomCharge.getCost();
+    }
+
+    @Override
+    public void setCost(double d) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCost'");
+    }
+}
+
+// InRoomPurchaseCharges class
+class InRoomPurchaseCharges implements BaseRoomCharge {
+    double cost;
+    BaseRoomCharge baseRoomCharge;
+
+    @Override
+    public Double getCost() {
+        baseRoomCharge.setCost(baseRoomCharge.getCost() + cost);
+        return baseRoomCharge.getCost();
+    }
+
+    @Override
+    public void setCost(double d) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCost'");
+    }
+}
+
+// Booking class
+class Booking {
+    RoomBooking createBooking(Guest guestInfo) {
+        // Implementation
+        return null;
+    }
+
+    RoomBooking cancelBooking(int bookingId) {
+        // Implementation
+        return null;
+    }
+}
+
+public class HotelManagementSystem {
+    public static void main(String[] args) {
+        // Optional: Test your classes and methods here
+    }
+}
