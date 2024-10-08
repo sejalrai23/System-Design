9f9a2a23325fb509f846471cc16e95bb7c15df26
Sejal Rai <sejalrai23@gmail.com>
2024-10-08 09:39:17 +0530

    Added vending machine
    Added vending machine

---
+++ Problems/VendingMachine/ItemType.java
@@ -0,0 +1,10 @@
+package Problems.VendingMachine;
+
+public enum ItemType {
+
+  COKE,
+  PEPSI,
+  JUICE,
+  SODA;
+}
+
---
+++ Problems/VendingMachine/VendingStates/State.java
@@ -0,0 +1,28 @@
+package Problems.VendingMachine.VendingStates;
+
+import java.util.*;
+import Problems.VendingMachine.Coin;
+import Problems.VendingMachine.Item;
+import Problems.VendingMachine.VendingMachine;
+
+public interface State {
+
+  public void clickOnInsertCoinButton(VendingMachine machine) throws Exception;
+
+  public void clickOnStartProductSelectionButton(VendingMachine machine) throws Exception;
+
+  public void insertCoin(VendingMachine machine , Coin coin) throws Exception;
+
+  public void chooseProduct(VendingMachine machine, int codeNumber) throws Exception;
+
+  public int getChange(int returnChangeMoney) throws Exception;
+
+  public Item dispenseProduct(VendingMachine machine, int codeNumber) throws Exception;
+
+  public List<Coin> refundFullMoney(VendingMachine machine) throws Exception;
+
+  public void updateInventory(VendingMachine machine, Item item, int codeNumber) throws Exception;
+
+
+}
+
